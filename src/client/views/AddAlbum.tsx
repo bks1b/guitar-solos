a915d6708103d57b92109797cacc5499ba16b3b7
bks1b <bks1bence@gmail.com>
2023-05-08 17:44:53 +0200

    Added Genius search feature

--- src/client/views/AddAlbum.tsx
+++ src/client/views/AddAlbum.tsx
@@ -1,9 +1,11 @@
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { MainContext } from '../util';
 
 export default () => {
     const { request, navigate } = useContext(MainContext)!;
     const refs = ['name', 'artist', 'year', 'cover'].map(k => [k, useRef<HTMLInputElement>(null)] as const);
+    const search = useRef<HTMLInputElement>(null);
+    const [results, setResults] = useState<string[][]>([]);
     useEffect(() => {
         document.title = 'Add album | Guitar Solos';
     }, []);
@@ -17,5 +19,8 @@ export default () => {
         <label>Cover URL: <input ref={refs[3][1]}/></label>
         <br/>
         <button onClick={() => request<{ id: string; }>('/add/album', Object.fromEntries(refs.map((x, i) => [x[0], i === 2 ? +x[1].current!.value : x[1].current!.value])), d => navigate(['album', d.id]))}>Add</button>
+        <hr/>
+        <input ref={search}/> <button onClick={() => request<string[][]>('/genius', { query: search.current!.value }, d => setResults(d))}>Search on Genius</button>
+        <ul>{results.map((x, i) => <li key={i}>{x[1]} - {x[0]}{x[2] ? ` (${x[2]})` : ''} <button onClick={() => refs.forEach((r, j) => r[1].current!.value = x[j] || '')}>Load</button></li>)}</ul>
     </>;
 };
