e98490d26d215c49b6ab01f6d1691322fcb9511c
bks1b <bks1bence@gmail.com>
2023-05-21 11:21:53 +0200

    Made error messages more user friendly

--- src/server/api.ts
+++ src/server/api.ts
@@ -47,9 +47,9 @@ export default Router()
         return [req.body[0].toLowerCase()];
     }))
     .post('/add/album', handler(req => {
-        if (typeof req.body?.name !== 'string' || !req.body.name.trim()) throw 'Album "name" expected.';
-        if (typeof req.body?.artist !== 'string' || !req.body.artist.trim()) throw 'Album "artist" expected.';
-        if (!checkInt(req.body?.year)) throw 'Album "year" expected to be a positive integer.';
+        if (typeof req.body?.name !== 'string' || !req.body.name.trim()) throw 'Name expected.';
+        if (typeof req.body?.artist !== 'string' || !req.body.artist.trim()) throw 'Artist expected.';
+        if (!checkInt(req.body?.year)) throw 'Year expected to be a positive integer.';
         return db.addAlbum({
             name: req.body.name.trim(),
             artist: req.body.artist.trim(),
@@ -58,10 +58,10 @@ export default Router()
         });
     }, true))
     .post('/add/song', handler(req => {
-        if (typeof req.body?.album !== 'string') throw 'Song "album" expected.';
-        if (typeof req.body?.name !== 'string' || !req.body.name.trim()) throw 'Song "name" expected.';
-        if (!Array.isArray(req.body?.genres) || !(req.body.genres as string[]).filter(x => x.trim()).length) throw 'Song "genres" expected.';
-        if (typeof req.body?.youtube !== 'string') throw 'Song "youtube" (video ID) expected.';
+        if (typeof req.body?.album !== 'string') throw 'Album expected.';
+        if (typeof req.body?.name !== 'string' || !req.body.name.trim()) throw 'Name expected.';
+        if (!Array.isArray(req.body?.genres) || !(req.body.genres as string[]).filter(x => x.trim()).length) throw 'Genres expected.';
+        if (typeof req.body?.youtube !== 'string') throw 'YouTube URL or ID expected.';
         const youtube = req.body.youtube.match(/(.*youtu\.be\/|.*[?&]v=)?([^?& ]+)/)?.[2];
         if (!youtube) throw 'Invalid YouTube URL or ID.';
         return db.addSong({
@@ -72,32 +72,32 @@ export default Router()
         });
     }, true))
     .post('/add/solo', handler(async req => {
-        if (typeof req.body?.song !== 'string') throw 'Solo "song" expected.';
-        if (!checkInt(req.body?.start)) throw 'Solo "start" expected to be a positive integer.';
-        if (!checkInt(req.body?.end)) throw 'Solo "end" expected to be a positive integer.';
+        if (typeof req.body?.song !== 'string') throw 'Song expected.';
+        if (!checkInt(req.body?.start)) throw 'Start expected to be a positive integer.';
+        if (!checkInt(req.body?.end)) throw 'End expected to be a positive integer.';
         await db.addSolo({ song: req.body.song, start: req.body.start, end: req.body.end });
     }, true))
     .post('/get/album', handler(async req => {
-        if (typeof req.body?.id !== 'string') throw 'Album "id" expected.';
+        if (typeof req.body?.id !== 'string') throw 'ID expected.';
         return db.getAlbum(req.body.id);
     }))
     .post('/get/song', handler(async req => {
-        if (typeof req.body?.id !== 'string') throw 'Song "id" expected.';
+        if (typeof req.body?.id !== 'string') throw 'ID expected.';
         return db.getSong(req.body.id, req.headers.authorization ? await getUser(getHeader(req)) : undefined);
     }))
     .post('/rate', handler(async (req, u) => {
-        if (typeof req.body?.id !== 'string') throw 'Rating "id" expected.';
-        if (!checkInt(req.body?.rating) || req.body.rating > 10) throw 'Rating "rating" expected to be an integer between 0 and 10 (inclusive).';
+        if (typeof req.body?.id !== 'string') throw 'ID expected.';
+        if (!checkInt(req.body?.rating) || req.body.rating > 10) throw 'Rating expected to be an integer between 0 and 10 (inclusive).';
         if (!await (await db.db).collection<Solo>('solos').findOne({ id: req.body.id })) throw 'Solo not found.';
         await db.editUser(getHeader(req), { $set: { ratings: [...u.ratings.filter(x => x.id !== req.body.id), { id: req.body.id, rating: req.body.rating }] } });
     }, true))
     .post('/discover', handler((_, u) => db.discover(u), true))
     .post('/profile', handler(async req => {
-        if (typeof req.body?.name !== 'string') throw 'User "name" expected.';
+        if (typeof req.body?.name !== 'string') throw 'Name expected.';
         return db.getProfile(req.body.name);
     }))
     .post('/search', handler(req => {
-        if (!req.body?.str || typeof req.body?.str !== 'string') throw 'Search "str" expected.';
+        if (!req.body?.str || typeof req.body?.str !== 'string') throw 'Query expected.';
         return db.search(req.body.str);
     }))
     .post('/charts', handler(() => db.getCharts()))
