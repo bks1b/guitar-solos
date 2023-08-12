c3fc0159c2fe93ca6f70a517df3cc159c661a529
bks1b <bks1bence@gmail.com>
2023-08-12 09:13:00 +0200

    Added a way to navigate to specific solos

--- src/client/components/Albums.tsx
+++ src/client/components/Albums.tsx
@@ -5,7 +5,7 @@ export default ({ arr, navigateArtist, ratings, album, ts }: { arr: Solos; navig
     const { navigateOnClick } = useContext(MainContext)!;
     return <div className='smallAlbums'>{arr.map((x, i) => <div key={i}>
         <img src={x[2].cover} className='link' {...navigateOnClick(['album', x[2].id])}/>
-        <h3 {...navigateOnClick([album ? 'album' : 'song', x[album ? 2 : 1].id])} className='link'>{x[album ? 2 : 1].name}</h3>
+        <h3 {...navigateOnClick([album ? 'album' : 'song', x[album ? 2 : 1].id], ts ? [['solo', x[0].id]] : [])} className='link'>{x[album ? 2 : 1].name}</h3>
         <h3 className='link' {...navigateArtist ? onClick(...navigateArtist(x[2].artist.toLowerCase())) : navigateOnClick([], [['artists', x[2].artist.toLowerCase()]])}>{x[2].artist}</h3>
         {ts ? <h4>{getTimestamp(x[0].start)}-{getTimestamp(x[0].end)}</h4> : ''}
         {ratings ? <h4>{x[3]}/10</h4> : ''}
