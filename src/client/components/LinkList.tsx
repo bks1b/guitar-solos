4b2e1d484347cea61f8500cba49ac26eb3bd27da
bks1b <bks1bence@gmail.com>
2023-12-03 17:56:53 +0100

    Added tags property to solos

--- src/client/components/LinkList.tsx
+++ src/client/components/LinkList.tsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
+import { Fragment, useContext } from 'react';
 import { MainContext } from '../util';
 
 export default ({ arr, query }: { arr: string[]; query: string; }) => {
     const { navigateOnClick } = useContext(MainContext)!;
-    return <>{arr.map((x, i) => <a key={i}><a className='link' {...navigateOnClick([], [[query, x.toLowerCase()]])}>{x}</a>{i < arr.length - 1 ? ', ' : ''}</a>)}</>;
+    return <>{arr.map((x, i) => <Fragment key={i}><a className='link' {...navigateOnClick([], [[query, x.toLowerCase()]])}>{x}</a>{i < arr.length - 1 ? ', ' : ''}</Fragment>)}</>;
 };
--- src/client/views/Admin.tsx
+++ src/client/views/Admin.tsx
@@ -18,25 +18,20 @@ export default () => {
                 a.download = 'solos.json';
                 a.click();
             })}>Download backup</button>
-            {data.unverified.some(x => x.length) ? <h1>Unverified entries</h1> : ''}
-            {data.unverified[0].length ? <h2>Albums</h2> : ''}
+            {data.unverified[0].length ? <h1>Unverified albums</h1> : ''}
             {
                 data.unverified[0].map((x, i) => <div key={i} className='adminAlbum'>
                     <img src={x.cover}/>
                     <div {...navigateOnClick(['album', x.id])} className='link'>{x.name} <a className='label'>by</a> {x.artist}</div>
                 </div>)
             }
-            {data.unverified[1].length ? <h2>Songs</h2> : ''}
+            {data.unverified[1].length ? <h1>Unverified songs</h1> : ''}
             <AdminSongs arr={data.unverified[1]}/>
-            {data.unverified[2].length ? <h2>Solos</h2> : ''}
+            {data.unverified[2].length ? <h1>Unverified solos</h1> : ''}
             {data.unverified[2].map((x, i) => <div key={i} {...navigateOnClick(['song', x[1].id], [['solo', x[0].id]])} className='link'>{getTimestamp(x[0].start)}-{getTimestamp(x[0].end)} <a className='label'>on</a> {x[1].name} <a className='label'>by</a> {x[2].artist}</div>)}
-            {data.noGuitarists.length ? <h1>Songs with solos without guitarists</h1> : ''}
-            <AdminSongs arr={data.noGuitarists}/>
+            {(['guitarists', 'tags'] as const).map(k => data[k].length ? <div key={k}><h1>Songs with solos without {k}</h1><AdminSongs arr={data[k]}/></div> : '')}
         </>
         : <></>;
 };
 
-type Data = {
-    unverified: [Album[], [Song, Album][], [Solo, Song, Album][]];
-    noGuitarists: [Song, Album][];
-};
+type Data = { unverified: [Album[], [Song, Album][], [Solo, Song, Album][]]; } & Record<'guitarists' | 'tags', [Song, Album][]>;
